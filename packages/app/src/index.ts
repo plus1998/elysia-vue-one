6c9051c6557447d33c38f5eceec7c344e5058088
happyplus <plus.1998@qq.com>
2025-07-25 10:21:49 +0800

    fix(app): 修复跨域请求问题
    - 将 @elysiajs/cors 版本改为 latest
    - 移除 bun-types devDependency
    - 在 onBeforeHandle 中手动设置跨域头，以解决 GET 请求跨域问题

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -4,6 +4,14 @@ import { auth } from "./modules/auth";
 
 const app = new Elysia()
   .use(cors())
+  // 因为cors对GET不起作用 应该是elysia的bug
+  .onBeforeHandle(async ({ set }) => {
+      set.headers = {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "*",
+          "Access-Control-Allow-Headers": "*",
+      }
+  })
   .use(auth)
   .get("/hi", "Hi, Elysia")
   .listen(3000);
